1bec1e8fb4f6b7f478fea66b726338811d787b50
Yelim <fla99916@gmail.com>
2024-07-02 23:25:09 +0200

    New view AddNewGroupOrderView

--- PhotoGo/AddNewGroupOrderView.swift
+++ PhotoGo/AddNewGroupOrderView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct AddNewGroupOrderView: View {
+    @State private var name: String = ""
+    @State private var owner: String = ""
+    @Environment(\.dismiss) var dismiss
+    
+    init() {
+        UILabel.appearance(whenContainedInInstancesOf: [UINavigationBar.self])
+            .adjustsFontSizeToFitWidth = true
+            
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section(header: Spacer(minLength: 0)) {} // ADDED TO ADJUST PADDING TOP
+
+                Section {
+                    TextField(
+                        "Group Order Name...",
+                        text: $name)
+                    TextField(
+                        "Group Order Owner...",
+                        text: $owner)
+
+                }
+            }
+            .padding(.top, -43) // = -63 + 20 (Padding vallue that I want)
+            .background(
+                Color(UIColor.systemGroupedBackground)
+            ) //Set the the above part color
+            .navigationTitle("Create New Group Order")
+            
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button(action: {}) { Text("Add") }
+                }
+                
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(action: { dismiss() }) {
+                        Image(systemName: "chevron.backward")
+                        Text("Cancel") }
+                }
+                
+            }
+        }
     }
 }
 
